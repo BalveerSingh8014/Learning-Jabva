cbf7513bc65a19c6689c4d2d31b2dbe1609caa5e
Balveer Singh <balveersinghyt@gmail.com>
2019-08-22 11:33:41 +0530

    Add files via upload

---
+++ inputfun.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class take
+{
+	
+}
+class inputfun
+{
+	public static void main(String s[])
+	{
+		System.out.println("Enter an integer value:- ");
+		Scanner sin = new Scanner(System.in);
+		int a;
+		a = sin.nextInt();
+		System.out.println("You Enter:- " + a);
+	}
+}
